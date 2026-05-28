fc05ee8577cd3257a7def68ba29c91d704016eec
dayronF <dayronflorez0@gmail.com>
2026-05-28 11:47:15 -0500

    feat: Se añade DTO de la tabla tareas al proyecto

--- src/main/java/com/task/Tasks_crud/dto/TaskMesagge.java
+++ src/main/java/com/task/Tasks_crud/dto/TaskMesagge.java
@@ -1,5 +1,13 @@
 package com.task.Tasks_crud.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskMesagge {
+    private String message;
     
 }
